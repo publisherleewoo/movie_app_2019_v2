5e952e8e41aaa32341b0e08b62ba258643bd6bbc
leewoo <dlguddn3214@naver.com>
2020-01-16 20:46:49 +0900

    Updates

---
+++ precache-manifest.754a244d3985559e1db875c136f4292b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91f951aaccb4e16ff86b486ce727a97",
+    "url": "/movie_app_2019_v2/index.html"
+  },
+  {
+    "revision": "ac206db9a01892ccb557",
+    "url": "/movie_app_2019_v2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7e935e2e50ffa55a4011",
+    "url": "/movie_app_2019_v2/static/js/2.e43b3062.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/movie_app_2019_v2/static/js/2.e43b3062.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac206db9a01892ccb557",
+    "url": "/movie_app_2019_v2/static/js/main.b7ba80b8.chunk.js"
+  },
+  {
+    "revision": "6c764583a3ad3699d121",
+    "url": "/movie_app_2019_v2/static/js/runtime-main.53f0fea8.js"
+  }
+]);
